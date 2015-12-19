91b351adafbf4c560e6a6c218c9eccd9f5ca04ef
Joseph Ramaswami <jramaswami@gmail.com>
2015-12-19 09:35:49 -0500

    Started to change code to process molecule as list instead of string but decided to change direction.

--- puzzle19/python/puzzle19.py
+++ puzzle19/python/puzzle19.py
@@ -3,8 +3,10 @@
 def transform_molecule(molecule, atomic_transformations):
     """
     Returns a Set of possible transformations
-    of the given molecule using the given dict
-    of possible atomic transformations.
+    of the given molecule, as a list of atoms,
+    using the given dict of possible atomic
+    transformations, where possible atomic
+    transformations should be lists as well.
     """
     transformations = []
     for index in range(len(molecule)):
@@ -18,6 +20,39 @@ def transform_molecule(molecule, atomic_transformations):
             transformations.append(possible_molecule)
 
     return set(transformations)
+
+def molecule_string_to_list(molecule_string):
+    """
+    Takes a molecule in string form and
+    returns it as a list of atoms.
+    """
+    molecule_list = []
+    partial = ''
+    # For each character in the string
+    for character in molecule_string:
+        # If the character is upper case then
+        # it is a whole molecule
+        if character == character.upper():
+            if partial != '':
+                # Add the previously partial atom,
+                # now complete, to the list
+                molecule_list.append(partial)
+                # Start a new partial atom
+                partial = character
+            else:
+                # This is the first character
+                partial = character
+        # If the character is lower case then
+        # it is part of the partial atom
+        else:
+            partial += character
+
+    # Don't forget to get the last partial atom
+    # that was being built, now complete
+    if partial:
+        molecule_list.append(partial)
+
+    return molecule_list
 
 def main():
     """Main application."""
--- puzzle19/python/test_puzzle19.py
+++ puzzle19/python/test_puzzle19.py
@@ -20,5 +20,27 @@ class TestPuzzle19(unittest.TestCase):
         new_molecule = p19.transform_molecule('H2O', transformations)
         self.assertEquals(set(['OO2O']), new_molecule)
 
+        # Same tests after string to list
+        transformations = {'H': ['HO', 'OH'], 'O': ['HH']}
+        molecule = p19.molecule_string_to_list('HOH')
+        new_molecules = p19.transform_molecule(molecule, transformations)
+        expected = set(['HOOH', 'HOHO', 'OHOH', 'HOOH', 'HHHH'])
+        self.assertEquals(expected, new_molecules)
+
+        molecule = p19.molecule_string_to_list('HOHOHO')
+        new_molecules = p19.transform_molecule(molecule, transformations)
+        self.assertEquals(7, len(new_molecules))
+
+        transformations = {'H': ['OO']}
+        molecule = p19.molecule_string_to_list('H2O')
+        new_molecule = p19.transform_molecule(molecule, transformations)
+        self.assertEquals(set(['OO2O']), new_molecule)
+
+    def test_molecule_string_to_list(self):
+        """Tests for molecule_string_to_list()"""
+        self.assertEquals(['H', 'O', 'H'], p19.molecule_string_to_list("HOH"))
+        self.assertEquals(['H', 'O', 'H', 'O', 'H', 'O'], \
+                          p19.molecule_string_to_list("HOHOHO"))
+
 if __name__ == '__main__':
     unittest.main()
