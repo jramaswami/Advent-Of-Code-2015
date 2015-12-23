fb50f81f02911100c1fef3f22c78702d078e1302
Joseph Ramaswami <jramaswami@gmail.com>
2015-12-22 22:35:27 -0500

    Solved the puzzle.

--- puzzle22/python/rpg.py
+++ puzzle22/python/rpg.py
@@ -1,6 +1,7 @@
 """RPG Simulation components of Day 22 Puzzle."""
 
 VERBOSE = False
+HARDMODE = False
 
 import magic
 
@@ -8,6 +9,14 @@ def log(msg):
     """Prints message if VERBOSE == True"""
     if VERBOSE:
         print msg
+
+class SpellCastingException(Exception):
+    """Exception for trying to cast a spell you can't"""
+    def __init__(self, value):
+        self.value = value
+
+    def __stri__(self):
+        return repr(self.value)
 
 class Character(object):
     """Represents a character in the game."""
@@ -18,10 +27,6 @@ class Character(object):
         self.damage = damage
         self.armor = armor
         self.mana = mana
-        if mana > 0:
-            self.spell_book = magic.SpellBook()
-        else:
-            self.spell_book = None
 
         self.original_hit_points = hit_points
         self.original_damage = damage
@@ -52,144 +57,87 @@ class Character(object):
 
     def can_cast_spell(self, spell_name):
         """Can character cast requested spell?"""
-        return self.spell_book.get_spell_cost(spell_name) <= self.mana
-
-    def cast_spell(self, spell_name, target):
-        """Cast the spell"""
-        if self.spell_book != None \
-        and self.spell_book.get_spell_cost(spell_name) <= self.mana:
-            spell = self.spell_book.get_spell(spell_name)
-            spell_effect = spell.cast()
-            self.mana -= spell.cost
-            spell_effect.activate(self, target)
-            return spell_effect
-
-def execute_effects(effects):
-    """Executes all the effects."""
-    # execute all active effects
-    for effect in effects:
-        effect.execute()
-        log('Executed effects of %s' % effect.spell_name)
-        log(str(effect))
-
-        if effect.timer == 0:
-            log("%s expired." % effect.spell_name)
-            effect.deactivate()
-
-
-    # remove expired items
-    effects[:] = [e for e in effects if e.timer > 0]
-
-def simulate_combat_round(player, boss, effects, player_spell):
-    """Simulates a single round of combat."""
-
-    # player turn
-    log("\n-- Player Turn --")
-    log(str(player))
-    log(str(boss))
-    execute_effects(effects)
-    if boss.is_dead():
-        log('Boss is dead!')
-        return
-    if player_spell != None and player.can_cast_spell(player_spell):
-        log(str(effects))
-        log('Player casting %s.' % player_spell)
-        new_spell_effect = player.cast_spell(player_spell, boss)
-        effects.append(new_spell_effect)
-    else:
-        log('Player unable to cast %s, not enough mana.' % player_spell)
-
-    # boss turn effects first
-    log("\n-- Boss Turn --")
-    log(str(player))
-    log(str(boss))
-    execute_effects(effects)
-    if boss.is_dead():
-        log('Boss is dead!')
-        log(str(boss))
-        log(str(player))
-        return
-    attack_damage = boss.attack(player)
-    log('Boss attacked player for %d damage.' % attack_damage)
-    log('Player now has %d hp.' % player.hit_points)
-    if player.is_dead():
-        log('Player is dead!')
-        log(str(boss))
-        log(str(player))
-        return
-
-    log(str(boss))
-    log(str(player))
-
-def simulate_combat(player, boss, player_spell_iterator, effects=[]):
-    """Simluate combat"""
-
-    log('Beginning combat ...')
-    log(str(player))
-    log(str(boss))
-
-    # player_spell_iterator.set_environ(player, effects)
-    while player.hit_points > 0 and boss.hit_points > 0:
-        try:
-            player_spell = player_spell_iterator.next()
-        except StopIteration:
-            return 0
-        simulate_combat_round(player, boss, effects, player_spell)
-        if boss.is_dead():
-            return 1
-        if player.is_dead():
-            return -1
+        return magic.get_spell_cost(spell_name) <= self.mana
 
 def spell_can_be_cast(spell, player, effects):
     """Function to say if spell can be cast."""
-    spells_in_effect = [e.spell_name for e in effects]
-    if spell in spells_in_effect:
+    spells_in_effect = effects.keys()
+    if spell in spells_in_effect and effects[spell] > 0:
         return False
     elif not player.can_cast_spell(spell):
         return False
     else:
         return True
 
-def simulate_combat_round_with_functions(player, boss, spell, effects):
+def execute_effects(player, boss, effects):
+    """Execute the effects"""
+    for effect, timer in effects.items():
+        log('Execute effect %s (%d)' % (effect, (timer - 1)))
+        effect_function = magic.EFFECTS[effect]
+        effect_function(player, boss)
+        effects[effect] = timer - 1
+        if timer - 1 == 0:
+            log('Effect %s expiring' % effect)
+            if effect == 'Shield':
+                log('Turning off shield')
+                magic.shield_expire(player, boss)
+            del effects[effect]
+
+def simulate_combat_round(player, boss, effects, spell):
+    """Simulate combat"""
+    # hardmode
+    if HARDMODE:
+        player.hit_points = player.hit_points - 1
+
     # player turn
     log("\n-- Player Turn --")
     log(str(player))
     log(str(boss))
 
-    for effect in effects:
-        effect(player, boss)
-
+    execute_effects(player, boss, effects)
     if boss.is_dead():
         log('Boss is dead!')
         return
 
     # Cast spell
-    spell(player, boss, effects)
+    log('Player casting spell %s' % spell)
+    spell_function = magic.SPELLBOOK[spell]['func']
+    spell_cost = magic.SPELLBOOK[spell]['cost']
+    log('Player spell costs %d mana' % spell_cost)
+    if spell_can_be_cast(spell, player, effects):
+        player.mana = player.mana - spell_cost
+        spell_function(player, boss, effects)
+    else:
+        raise SpellCastingException('Unable to cast spell %s')
 
     # boss turn effects first
     log("\n-- Boss Turn --")
     log(str(player))
     log(str(boss))
-    execute_effects(effects)
+
+    execute_effects(player, boss, effects)
+
     if boss.is_dead():
         log('Boss is dead!')
-        log(str(boss))
         log(str(player))
-        return
+        log(str(boss))
+        return 1
+
     attack_damage = boss.attack(player)
     log('Boss attacked player for %d damage.' % attack_damage)
     log('Player now has %d hp.' % player.hit_points)
+
     if player.is_dead():
         log('Player is dead!')
-        log(str(boss))
         log(str(player))
-        return
+        log(str(boss))
+        return -1
 
-    log(str(boss))
     log(str(player))
+    log(str(boss))
 
-
-def simulate_combat_with_functions(boss, player, spells, effects):
+def simulate_combat(player, boss, spells, effects):
+    """Simulate combat"""
     log('Beginning combat ...')
     log(str(player))
     log(str(boss))
@@ -199,8 +147,9 @@ def simulate_combat_with_functions(boss, player, spells, effects):
             player_spell = spells.next()
         except StopIteration:
             return 0
-        simulate_combat_round_with_functions(player, boss, effects, player_spell)
-        if boss.is_dead():
-            return 1
-        if player.is_dead():
-            return -1
+        simulate_combat_round(player, boss, effects, player_spell)
+
+    if boss.is_dead():
+        return 1
+    elif player.is_dead():
+        return -1
