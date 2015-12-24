63cfd9a783913dedbaeea30d14644dd1b7d79543
Joseph Ramaswami <jramaswami@gmail.com>
2015-12-24 11:21:52 -0500

    Savepoint.

--- puzzle24/python/puzzle24.py
+++ puzzle24/python/puzzle24.py
@@ -25,32 +25,56 @@ def log(message):
         print message
 
 import itertools as it
-def find_groups(packages, total_weight, early_exit=True):
+def find_groups(packages, total_weight, group_number=1, early_exit=True):
     """Find a group from packages with a given total weight."""
-    solutions = []
+    solutions = set([])
     # Starting from a length of 1, iterate through all the
     # combinations of packages of the given length.
     for group_length in xrange(1, len(packages)):
-        log('Checking groups of length %d that have total weight of %d' \
-                % (group_length, total_weight))
+        # log('Checking groups of length %d that have total weight of %d' \
+                # % (group_length, total_weight))
         for group in it.combinations(packages, group_length):
-            # If a group of packages has a weight equal to
-            # required weight, verify that you can split
-            # the remaining packages into a groups of
-            # the required weight.
+
+            # If group weighs the required amount, we have a
+            # potential solution ...
             if sum(group) == total_weight:
-                log('Found potential solution: %s' % str(group))
-                log('Verifiying remaining packages ...')
-                remaining_packages = [p for p in packages if p not in group]
-                # If there is a group within the remaining packages
-                # that is of the required weight, then the third
-                # package must be of the required weight because the
-                # required weight is the weight of all the packages
-                # divided by three. So if thre is at least one
-                # group of the required weight, initial group to
-                # our list of solutions.
-                if len(find_groups(remaining_packages, total_weight)) > 0:
-                    solutions.append(group)
+                # If we are in group 1, then we must verify that
+                # the remaining packages can be split into groups
+                # of the required weight.
+                if group_number == 1:
+                    log('Found potential group %d: %s --> %d' \
+                        % (group_number, str(group), sum(group)))
+                    log('Verifiying remaining packages ...')
+                    remaining_packages = [p for p in packages if p not in group]
+                    remaining_groups = find_groups(remaining_packages, \
+                                                   total_weight, \
+                                                   group_number=group_number+1)
+                    if len(remaining_groups) > 0:
+                        # There are solutions in the remaining packages
+                        # so, append the group to the list of solutions
+                        group1 = tuple(sorted(group, reverse=True))
+                        group2 = tuple(sorted(remaining_groups[0], reverse=True))
+                        group3 = tuple(sorted(remaining_groups[1], reverse=True))
+                        solution_groups = sorted([group1, group2, group3], key=len)
+
+                        solutions.add(tuple(solution_groups))
+                # We are verifying that there are solutions for group 2
+                # and group 3.  If there is a solution to group 2, then
+                # there must be a solution to group 3 since the required
+                # weight is total weight divided by 3 and we have found
+                # two groups of the required weight:
+                #       total_weight = 3 * group_weight
+                # We can just return the single solution because we
+                # just want to verify.
+                else:
+                    log('Verified group %d solution: %s --> %d' \
+                        % (group_number, str(group), sum(group)))
+                    remaining_packages = [p for p in packages if p not in group]
+                    log('Verified group %d solution: %s --> %d' \
+                        % (group_number + 1, str(remaining_packages), \
+                           sum(remaining_packages)))
+                    log("*" * 20)
+                    return [group, tuple(remaining_packages)]
 
         # If an early exit has been requested and
         # if we have solutions then we have found the
