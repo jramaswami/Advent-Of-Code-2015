ffeaadb541cb167d7447db160001c9915c30e1f6
Joseph Ramaswami <jramaswami@gmail.com>
2015-12-24 11:43:04 -0500

    Added test for early exit.

--- puzzle24/python/test_puzzle24.py
+++ puzzle24/python/test_puzzle24.py
@@ -8,23 +8,22 @@ class TestPuzzle24(unittest.TestCase):
 
     def test_something(self):
         """Test find_groups()"""
-        p24.DEBUG = True
+        p24.DEBUG = False
         packages = range(1, 6)
         packages.extend(range(7, 12))
         packages.reverse
 
         weight = sum(packages) / 3
         solutions = p24.find_groups(packages, weight, early_exit=False)
-        print "*" * 80
-        for t in solutions:
-            print t
-
         possibles = [[11, 9], [10, 9, 1], [10, 8, 2], [10, 7, 3], \
                      [10, 5, 4, 1], [10, 5, 3, 2], [10, 4, 3, 2, 1], \
                      [9, 8, 3], [9, 7, 4], [9, 5, 4, 2], [8, 7, 5], \
                      [8, 5, 4, 3], [7, 5, 4, 3, 1]]
         for poss in possibles:
             self.assertIn(poss, solutions)
+
+        solutions = p24.find_groups(packages, weight, early_exit=True)
+        self.assertEquals([[11, 9]], solutions)
 
 if __name__ == "__main__":
     unittest.main()
