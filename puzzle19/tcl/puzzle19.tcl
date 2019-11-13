15da83cfd8a4b7d6c866aa71b6aabda1de51a25e
Joseph Ramaswami <jramaswami@gmail.com>
2019-11-13 16:36:27 -0500

    Solution to day 19 in Tcl.

--- puzzle19/tcl/puzzle19.tcl
+++ puzzle19/tcl/puzzle19.tcl
@@ -6,6 +6,7 @@ package require struct::set
 # and the second item is a list of transformations.  Each transformation
 # is a list of two items: the from string and the to string.
 proc parse_input {input} {
+    set transformations [dict create]
     set lines [split $input "\n"]
     set num_lines [llength $lines]
     set molecule [lindex $lines end]
@@ -16,30 +17,35 @@ proc parse_input {input} {
         set tokens [split $line]
         set from [lindex $tokens 0]
         set to [lindex $tokens 2]
-        lappend transformations [list $from $to]
+        dict lappend transformations $from $to
     }
     return [list $molecule $transformations]
 }
 
 # Given a molecule and a transformation, this proc returns a set of 
 # all possible new molecules that can be created with the given
-# transformation.
-proc possible_molecules {molecule transformation} {
+# transformations.
+proc possible_molecules {molecule transformations} {
     set possibles {}
-    set start_search 0
-    set search_limit [string length $molecule]
-    set from [lindex $transformation 0]
-    set from_length [string length $from]
-    set to [lindex $transformation 1]
-    set to_length [string length $to]
+    set molecule_length [string length $molecule]
 
-    set index [string first $from $molecule $start_search]
-    while {$index >= 0 && $index < $search_limit} {
-        set index_end [expr {$index + $from_length - 1}]
-        set molecule0 [string replace $molecule $index $index_end $to]
-        ::struct::set include possibles $molecule0
-        set start_search [expr {$index + 1}]
-        set index [string first $from $molecule $start_search]
+    set index 0
+    while {$index < $molecule_length} {
+        # Atoms are either a single capital letter or a capital followed by a
+        # lowercase letter.
+        set atom [string index $molecule $index]
+        set atom0 [string index $molecule [expr {$index + 1}]]
+        if {[string is lower $atom0]} {
+            set atom [string cat $atom $atom0]
+        }
+        if {[dict exists $transformations $atom]} {
+            set lhs [string range $molecule 0 [expr {$index - 1}]]
+            set rhs [string range $molecule [expr {$index + [string length $atom]}] end]
+            foreach replacement [dict get $transformations $atom] {
+                ::struct::set include possibles [string cat $lhs $replacement $rhs]
+            }
+        }
+        incr index [string length $atom]
     }
     return $possibles
 }
@@ -47,83 +53,38 @@ proc possible_molecules {molecule transformation} {
 # Return the number of unique molecules that can be formed by one round
 # of all the given transformations.
 proc solve_part1 {molecule transformations} {
-    set possibles {}
-    foreach transformation $transformations {
-        set possibles0 [possible_molecules $molecule $transformation]
-        ::struct::set add possibles $possibles0
-    }
+    set possibles [possible_molecules $molecule $transformations]
     return [::struct::set size $possibles]
 }
 
-proc reverse_transformations {transformations} {
-    for {set i 0} {$i < [llength $transformations]} {incr i} {
-        set t [lindex $transformations $i]
-        set from [lindex $t 0]
-        set to [lindex $t 1]
-        set t0 [list $to $from]
-        lset transformations $i $t0
-    }
-    return $transformations
-}
-
-proc solve_part2_in_reverse {molecule transformations} {
-    set transformations0 [reverse_transformations $transformations]
+# Based on the solutoin of u/askalski
+# https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju/
+proc solve_part2 {molecule} {
     set molecule_length [string length $molecule]
-    ::struct::set include visited $molecule
-    ::struct::set include queue $molecule
-    set ticks 0
-    while {1} {
-        incr ticks
-        foreach transformation $transformations0 {
-            foreach medicine $queue {
-                set possibles [possible_molecules $medicine $transformation]
-                foreach possible $possibles {
-                    if {[::struct::set contains $visited $possible]} {
-                        continue
-                    }
-                    if {[string compare e $medicine] == 0} {
-                        return $ticks
-                    }
-                    ::struct::set include visited $possible
-                    ::struct::set include new_queue $possible
-                }
-            }
+    set atoms {}
+    set atom_count 0
+    set atom_Ar_Rn_count 0
+    set atom_Y_count 0
+    set index 0
+    while {$index < $molecule_length} {
+        # Atoms are either a single capital letter or a capital followed by a
+        # lowercase letter.
+        set atom [string index $molecule $index]
+        set atom0 [string index $molecule [expr {$index + 1}]]
+        if {[string is lower $atom0]} {
+            set atom [string cat $atom $atom0]
         }
-        puts "$ticks [::struct::set size $new_queue]"
-        set queue $new_queue
-        set new_queue {}
-    }
-}
-
-proc solve_part2 {molecule transformations} {
-    set molecule_length [string length $molecule]
-    ::struct::set include visited e
-    ::struct::set include queue e
-    set ticks 0
-    while {1} {
-        incr ticks
-        foreach transformation $transformations {
-            foreach medicine $queue {
-                set possibles [possible_molecules $medicine $transformation]
-                foreach possible $possibles {
-                    if {[string length $possible] > $molecule_length} {
-                        continue
-                    }
-                    if {[::struct::set contains $visited $possible]} {
-                        continue
-                    }
-                    if {[string compare $molecule $medicine] == 0} {
-                        return $ticks
-                    }
-                    ::struct::set include visited $possible
-                    ::struct::set include new_queue $possible
-                }
-            }
+        lappend atoms $atom
+        incr atom_count
+        if {$atom == "Ar" || $atom == "Rn"} {
+            incr atom_Ar_Rn_count
         }
-        puts "$ticks [::struct::set size $new_queue]"
-        set queue $new_queue
-        set new_queue {}
+        if {$atom == Y} {
+            incr atom_Y_count
+        }
+        incr index [string length $atom]
     }
+    return [expr {$atom_count - $atom_Ar_Rn_count - (2 * $atom_Y_count) - 1}]
 }
 
 if {!$tcl_interactive} {
@@ -132,5 +93,5 @@ if {!$tcl_interactive} {
     set molecule [lindex $parsed 0]
     set transformations [lindex $parsed 1]
     puts "The solution to part 1 is [solve_part1 $molecule $transformations]."
-    puts "The solution to part 2 is [solve_part2_in_reverse $molecule $transformations]."
+    puts "The solution to part 2 is [solve_part2 $molecule]."
 }
